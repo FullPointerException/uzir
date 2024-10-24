17404150471f10268589b22336c2697b7c6233e6
Full Pointer Exception <fullpointerxception@gmail.com>
2024-10-23 19:40:18 -0500

    Basic gamestate enum implementation

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,8 @@ const std = @import("std");
 
 const raylib = @import("raylib");
 
+const MainState = enum { logo, main_menu, town, battle, rewards };
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
@@ -24,25 +26,50 @@ pub fn main() !void {
     defer raylib.closeWindow();
     raylib.setTargetFPS(60);
 
-    while(!raylib.windowShouldClose()) {
-        raylib.beginDrawing();
-        defer raylib.endDrawing();
+    var state = MainState.logo;
 
-        raylib.clearBackground(raylib.Color.white);
-        raylib.drawText("UZIR", 30, 30, 10, raylib.Color.light_gray);
+    while (!raylib.windowShouldClose()) {
+        state = updateFrame(state);
+        drawFrame(state);
     }
-
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn updateFrame(s: MainState) MainState {
+    const newState = switch (s) {
+        MainState.logo => MainState.main_menu,
+        MainState.main_menu => MainState.town,
+        MainState.town => MainState.battle,
+        MainState.battle => MainState.rewards,
+        MainState.rewards => MainState.logo,
+    };
+
+    return newState;
 }
 
-test "fuzz example" {
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-    const input_bytes = std.testing.fuzzInput(.{});
-    try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input_bytes));
+pub fn drawFrame(s: MainState) void {
+    raylib.beginDrawing();
+    defer raylib.endDrawing();
+
+    raylib.clearBackground(raylib.Color.white);
+
+    switch (s) {
+        MainState.logo => raylib.drawText("uzir", 30, 30, 10, raylib.Color.gray),
+        MainState.main_menu => raylib.drawText("Uzir", 30, 30, 10, raylib.Color.gray),
+        MainState.town => raylib.drawText("uZir", 30, 30, 10, raylib.Color.gray),
+        MainState.battle => raylib.drawText("uzIr", 30, 30, 10, raylib.Color.gray),
+        MainState.rewards => raylib.drawText("uziR", 30, 30, 10, raylib.Color.gray),
+    }
 }
+
+//test "simple test" {
+//    var list = std.ArrayList(i32).init(std.testing.allocator);
+//    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
+//    try list.append(42);
+//    try std.testing.expectEqual(@as(i32, 42), list.pop());
+//}
+
+// test "fuzz example" {
+// Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+//    const input_bytes = std.testing.fuzzInput(.{});
+//    try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input_bytes));
+//}
